1bfaaba0c51838fdb1bb2a9f693fce31555c2ce0
deancureton <deancureton@westminster.net>
2022-02-14 09:52:56 -0500

    Clean up LexerTest

--- Sigma/2 - Lexical Analysis/src/com/sigma/lexicalAnalysis/LexerTest.java
+++ Sigma/2 - Lexical Analysis/src/com/sigma/lexicalAnalysis/LexerTest.java
@@ -9,39 +9,29 @@ import java.util.ArrayList;
 import static com.sigma.lexicalAnalysis.TokenType.*;
 
 public class LexerTest {
+    public static void main(String[] args) throws IOException {
+        try {
+            if (args.length == 1) runFile(args[0]);
+            else {
+                System.out.println("Usage: sigma [path to .sigma file]");
+                System.exit(64);
+            }
+        } catch (IOException e) {
+            throw new IOException(e.toString());
+        }
+    }
+
     public static void runFile(String path) throws IOException {
+        if (!path.endsWith(".sigma")) path += ".sigma";
+        System.out.println("Running " + path + "...");
         String sourceCode = getSourceCodeFromFile(path);
-        run(sourceCode);
-
-        //if (hadSyntaxError) System.exit(65);
-        //if (hadRuntimeError) System.exit(70);
-    }
-
-    private static String getSourceCodeFromFile(String path) throws IOException {
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
-        return new String(bytes, Charset.defaultCharset());
-    }
-
-    public static void run(String sourceCode) {
         Lexer lexer = new Lexer(sourceCode);
         ArrayList<Lexeme> lexemes = lexer.lex();
         lexer.printLexemes();
     }
 
-    private static boolean singlePathProvided(String[] args) {
-        if (args.length == 1) return true;
-        return false;
-    }
-
-    public static void main(String[] args) throws IOException {
-        try {
-            if (singlePathProvided(args)) runFile(args[0]);
-            else {
-                System.out.println("Usage: sigma [path to .sigma file]");
-                System.exit(64);
-            }
-        } catch (IOException exception) {
-            throw new IOException(exception.toString());
-        }
+    private static String getSourceCodeFromFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        return new String(bytes, Charset.defaultCharset());
     }
 }
